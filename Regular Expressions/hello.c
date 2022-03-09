99e0c09a165abebceedbc1ae1da9dea0439d32bd
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-03-09 17:34:48 -0600

    Create NoComments class and hello.c test file

---
+++ Regular Expressions/hello.c
@@ -0,0 +1,13 @@
+/* Este es un
+ejemplo de
+un comentario
+con múltiples líneas. */
+
+#include <stdio.h>
+
+int main(void) { /* función principal */
+	char _ = 'a';
+	printf("Hola\n");
+	return 0;
+}
+/* fin */
---
+++ Regular Expressions/no_comments.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class NoComments {
+
+    public static void Main() {
+        var regex = new Regex(@"([/][*].*?[*][/])|(.)", RegexOptions.Singleline);
+        var text = File.ReadAllText("hello.c");
+        foreach (Match match in regex.Matches(text)) {
+            if (match.Groups[1].Success) {
+                // found comment, do nothing
+            } else {
+                Console.Write(match.Value);
+            }
+        }
+    }
+}
